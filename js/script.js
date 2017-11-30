c257aa7a7403042b0a8d2b5a3ee4b82dcfae6688
Dawn Joseph <dawnjoseph4747@gmail.com>
2017-11-30 05:48:04 +0000

    third push

--- js/script.js
+++ js/script.js
@@ -1,31 +1,20 @@
 //Gallery
-  $(document).ready(function(){
-
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
-    
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
-
+// Created by Avaz Bokiev @samarkandiy
+$(document).ready(function(){
+  $("[class^='thumbnail-']").click(function(){
+    $("[class^='thumbnail-']").slideToggle("fast");
+    $(this).next("[class^='large-']").slideToggle();
+  });
+  
+  $(".close").click(function(){
+    $("[class^='large-']:visible").toggle();
+    $("[class^='thumbnail-']").fadeToggle("fast");; 
+  }); 
+  
 });
 
+new WOW().init();
+  
 //Contact css
 
 /* form validation plugin */
